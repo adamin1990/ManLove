cc1e34fa243b9462e2b8d4e071f491caf2202391
XiaoPengLi <adamin1990@gmail.com>
2016-02-25 12:10:17 +0800

    update project,finish the mainfragment function

---
+++ adamintoolkits/src/main/java/adamin/toolkits/utils/KeywordUtil.java
@@ -0,0 +1,103 @@
+package adamin.toolkits.utils;
+
+/**
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ * //          佛曰:
+ * //                  写字楼里写字间，写字间里程序员；
+ * //                  程序人员写程序，又拿程序换酒钱。
+ * //                  酒醒只在网上坐，酒醉还来网下眠；
+ * //                  酒醉酒醒日复日，网上网下年复年。
+ * //                  但愿老死电脑间，不愿鞠躬老板前；
+ * //                  奔驰宝马贵者趣，公交自行程序员。
+ * //                  别人笑我忒疯癫，我笑自己命太贱；
+ * //                  不见满街漂亮妹，哪个归得程序员？
+ * //
+ * //         Created by LiTao on 2016-02-24-15:19.
+ * //         Company: QD24so
+ * //         Email: 14846869@qq.com
+ * //         WebSite: http://lixiaopeng.top
+ * //
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+
+/**
+ * 关键字高亮变色
+ */
+public class KeywordUtil {
+
+    /**
+     * 关键字高亮变色
+     *
+     * @param color
+     *            变化的色值
+     * @param text
+     *            文字
+     * @param keyword
+     *            文字中的关键字
+     * @return
+     */
+    public static SpannableString matcherSearchTitle(int color, String text,
+                                                     String keyword) {
+        SpannableString s = new SpannableString(text);
+        Pattern p = Pattern.compile(keyword);
+        Matcher m = p.matcher(s);
+        while (m.find()) {
+            int start = m.start();
+            int end = m.end();
+            s.setSpan(new ForegroundColorSpan(color), start, end,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        return s;
+    }
+
+    /**
+     * 多个关键字高亮变色
+     *
+     * @param color
+     *            变化的色值
+     * @param text
+     *            文字
+     * @param keyword
+     *            文字中的关键字数组
+     * @return
+     */
+    public static SpannableString matcherSearchTitle(int color, String text,
+                                                     String[] keyword) {
+        SpannableString s = new SpannableString(text);
+        for (int i = 0; i < keyword.length; i++) {
+            Pattern p = Pattern.compile(keyword[i]);
+            Matcher m = p.matcher(s);
+            while (m.find()) {
+                int start = m.start();
+                int end = m.end();
+                s.setSpan(new ForegroundColorSpan(color), start, end,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            }
+        }
+        return s;
+    }
+}
---
+++ adamintoolkits/src/main/java/adamin/toolkits/utils/StatusBarCompact.java
@@ -0,0 +1,105 @@
+package adamin.toolkits.utils;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖镇楼                  BUG辟易
+ * //          佛曰:
+ * //                  写字楼里写字间，写字间里程序员；
+ * //                  程序人员写程序，又拿程序换酒钱。
+ * //                  酒醒只在网上坐，酒醉还来网下眠；
+ * //                  酒醉酒醒日复日，网上网下年复年。
+ * //                  但愿老死电脑间，不愿鞠躬老板前；
+ * //                  奔驰宝马贵者趣，公交自行程序员。
+ * //                  别人笑我忒疯癫，我笑自己命太贱；
+ * //                  不见满街漂亮妹，哪个归得程序员？
+ * //
+ * //         Created by LiTao on 2016-02-24-16:29.
+ * //         Company: QD24so
+ * //         Email: 14846869@qq.com
+ * //         WebSite: http://lixiaopeng.top
+ * //
+ */
+public class StatusBarCompact {
+    private static final int INVALID_VAL = -1;
+    private static final int COLOR_DEFAULT = Color.parseColor("#20000000");
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static void compat(Activity activity, int statusColor)
+    {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            if (statusColor != INVALID_VAL)
+            {
+                activity.getWindow().setStatusBarColor(statusColor);
+            }
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+        {
+            int color = COLOR_DEFAULT;
+            ViewGroup contentView = (ViewGroup) activity.findViewById(android.R.id.content);
+            if (statusColor != INVALID_VAL)
+            {
+                color = statusColor;
+            }
+            View statusBarView = contentView.getChildAt(0);
+            //改变颜色时避免重复添加statusBarView
+            if (statusBarView != null && statusBarView.getMeasuredHeight() == getStatusBarHeight(activity))
+            {
+                statusBarView.setBackgroundColor(color);
+                return;
+            }
+            statusBarView = new View(activity);
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    getStatusBarHeight(activity));
+            statusBarView.setBackgroundColor(color);
+            contentView.addView(statusBarView, lp);
+        }
+
+    }
+
+    public static void compat(Activity activity)
+    {
+        compat(activity, INVALID_VAL);
+    }
+
+
+    public static int getStatusBarHeight(Context context)
+    {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0)
+        {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+}
