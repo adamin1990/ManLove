be43c7e6a55ea01a3ecffaf4fe43948d7b8333b4
XiaoPengLi <adamin1990@gmail.com>
2016-03-04 10:38:50 +0800

    replace finish with ActivityCompat.finishTrasition

--- app/src/main/java/com/adamin/manslove/view/detail/DetailActivity.java
+++ app/src/main/java/com/adamin/manslove/view/detail/DetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.EdgeEffectCompat;
 import android.support.v7.app.AlertDialog;
@@ -174,7 +175,7 @@ public class DetailActivity extends AppCompatActivity implements DetailView,Deta
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId()==android.R.id.home){
-            finish();
+            ActivityCompat.finishAfterTransition(this);
         }
         return super.onOptionsItemSelected(item);
     }
