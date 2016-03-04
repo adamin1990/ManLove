7f0117b6cd985b2e178a7cfeacee7836c6819c1a
XiaoPengLi <adamin1990@gmail.com>
2016-03-04 10:06:37 +0800

    add superrecyclerview model

---
+++ superrecyclerview/src/main/java/com/adamin/superrecyclerview/superrecycer/OnMoreListener.java
@@ -0,0 +1,10 @@
+package com.adamin.superrecyclerview.superrecycer;
+
+public interface OnMoreListener {
+    /**
+     * @param overallItemsCount
+     * @param itemsBeforeMore
+     * @param maxLastVisiblePosition for staggered grid this is max of all spans
+     */
+    void onMoreAsked(int overallItemsCount, int itemsBeforeMore, int maxLastVisiblePosition);
+}
